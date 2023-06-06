a38a16cafb5a212087bfe438aa5107036159b482
Jr Bytes <itamazor@gmail.com>
2023-06-06 10:57:07 -0300

    test: ensure Message paragraph correct color and size

--- src/components/Message/Message.spec.tsx
+++ src/components/Message/Message.spec.tsx
@@ -5,13 +5,19 @@ import { Message, MessageType } from '../../components/Message'
 describe('Message', () => {
   test('should render success Message', async () => {
     render(<Message type={MessageType.SUCCESS} value="Mensagem" />)
-    expect(screen.getByRole('heading', { name: /sucesso/i }))
-    expect(screen.getByText(/mensagem/i))
+    expect(
+      screen.getByRole('heading', { name: /sucesso/i }),
+    ).toBeInTheDocument()
+    const message = screen.getByText(/mensagem/i)
+    expect(message).toBeInTheDocument()
+    expect(message).toHaveClass('text-sm', 'text-tertiaryDark')
   })
 
   test('should render error Message', async () => {
     render(<Message type={MessageType.ERROR} value="Mensagem" />)
-    expect(screen.getByRole('heading', { name: /erro/i }))
-    expect(screen.getByText(/mensagem/i))
+    expect(screen.getByRole('heading', { name: /erro/i })).toBeInTheDocument()
+    const message = screen.getByText(/mensagem/i)
+    expect(message).toBeInTheDocument()
+    expect(message).toHaveClass('text-sm', 'text-white')
   })
 })
